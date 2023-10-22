1d9d6a49a6aa724aac10a0e14d7aea86059e8cef
Andreas Kiesel <andreas.kiesel@sendev.gmbh>
2023-10-22 17:18:40 +0200

    added hover animation for img

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,7 +7,7 @@ const Clients = () => (
       {clients.map((client) => (
         <div
           key={client.id}
-          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5 transform hover:scale-120 transition-transform duration-100 cursor-pointer`}
         >
           <img
             src={client.logo}
